1a65ab9502a74635c9f170cc2bcf862891434f06
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-09-10 19:16:09 +0530

    Youtube clone created using material ui and reactjs

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: '50'
+  },
+  headers: {
+    'X-RapidAPI-Key': '5cf5cb97e3mshae37258832a8671p1b447cjsn5b326adbea16',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+}
